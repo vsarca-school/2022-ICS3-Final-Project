84fe17de550a9e42ea420e4a3ea5f28f4c5a171a
vsarca <vsarca@outlook.com>
2023-01-23 17:06:33 -0500

    Fixed text box size

--- Code Editor.js
+++ Code Editor.js
@@ -1,7 +1,12 @@
 {
     let content = document.currentScript.parentElement;
     let textarea = content.getElementsByTagName("textarea")[0];
-    //let a = content.getElementsByTagName("a")[0];
+    
+    //textarea.rows = window.innerHeight / 20;
+    window.onresize = function () {
+        textarea.rows = Math.floor(window.innerHeight / 22);
+    }
+
     let button = content.getElementsByTagName("button")[0];
     button.onclick = function () {
         eval(textarea.value);
