cab26c6b3a2c29f28179afea0143303cdecd732d
vsarca <vsarca@outlook.com>
2023-01-22 23:16:11 -0500

    code editor

--- Code Editor.js
+++ Code Editor.js
@@ -6,18 +6,4 @@
     button.onclick = function () {
         eval(textarea.value);
     }
-    
-    /*a.onclick = function(e) {
-        e.preventDefault();
-        input.click();
-    };
-    let input = document.createElement("input");
-    input.type = "file";
-    input.onchange = function(e) {
-        let reader = new FileReader();
-        reader.onload = function () {
-            nn = new NeuralNetwork(1,1).fromFile(JSON.parse(reader.result));
-        }
-        reader.readAsText(input.files[0]);
-    }*/
 }
